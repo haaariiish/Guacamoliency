703936255bb4165ee0cb43ad0052127c8ce08078
haaariiish <harishprabakaranpro@gmail.com>
2025-05-06 15:09:54 +0200

    snakemake

--- launch_training_moses.sh
+++ launch_training_moses.sh
@@ -1,6 +1,7 @@
 # Launch training 
 
-python guacamoliency/modeling/train.py  --datasets moses --output_dir reports
+#python guacamoliency/modeling/train.py  --datasets moses --output_dir reports
 
 #python guacamoliency/modeling/train.py  --datasets guacamol --output_dir results/guacamol
 
+python run_workflow.py
---
+++ run_workflow.py
@@ -0,0 +1,15 @@
+from snakemake import snakemake
+
+success = snakemake(
+    snakefile="workflow.smk",          # chemin vers ton Snakefile
+    cores=4,                        # nombre de cœurs à utiliser
+    workdir=".",                    # répertoire de travail
+    use_conda=False,                # active conda si besoin
+    verbose=True,                   # verbosité
+    dryrun=True                    # True pour tester sans exécuter
+)
+
+if success:
+    print("Pipeline exécuté avec succès.")
+else:
+    print("Erreur dans le pipeline.")
