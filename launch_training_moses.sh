07f34c19579e7c53427d934714b3704b8143924d
haaariiish <harishprabakaranpro@gmail.com>
2025-05-06 14:50:51 +0200

    update du snakemakefile

--- launch_training_moses.sh
+++ launch_training_moses.sh
@@ -1,6 +1,5 @@
 # Launch training 
 
-tensorboard --logdir=args./logs
 python guacamoliency/modeling/train.py  --datasets moses --output_dir reports
 
 #python guacamoliency/modeling/train.py  --datasets guacamol --output_dir results/guacamol
