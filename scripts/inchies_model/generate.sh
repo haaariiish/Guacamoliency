a7d039e26be34041e39865bfcab27681e8314d39
haaariiish <harishprabakaranpro@gmail.com>
2025-07-22 13:51:40 +0200

    cleaning scripts

--- scripts/inchies_model/generate.sh
+++
@@ -1,6 +0,0 @@
-python guacamoliency/modeling/generate.py \
-    --model moses_inchies \
-    --model_dir 'models/trained_moses_canonical_INCHIES/1/final_model' \
-    --num_sequence 10000 \
-    --temperature 1 \
-    --output_dir "data/generated/moses_canonical_INCHIES_1.csv"
