da97236dcc3ca8b42e70516d6f65148062a2c186
haaariiish <harishprabakaranpro@gmail.com>
2025-07-23 15:10:12 +0200

    scripts for block smiles training

---
+++ train_blocksmiles.sh
@@ -0,0 +1,13 @@
+python guacamoliency/modeling/train.py \
+    --tokenizer_path data/tokenizers_blocksmiles/moses_canonical \
+    --tokenizer_type "simple_blocks" \
+    --datasets blocksmiles_simple\
+    --log_dir reports \
+    --dataset_dir data/training_data/blocksmiles_simple.csv \
+    --model_save_folder models/trained_moses_blocksmiles_simple \
+    --learning_rate 6e-4 \
+    --max_steps 41300 \
+    --batch_size 384 \
+    --save_steps 41300\
+    --save_total_limit 10\
+    --warmup_steps 412
